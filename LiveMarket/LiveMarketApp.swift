cb876b15694727cd27ebafb0ee1b86fe0002b3e9
Mariusz Zając <zajac@gmx.us>
2022-11-18 17:44:16 +0100

    Initial Commit

---
+++ LiveMarket/LiveMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveMarketApp.swift
+//  LiveMarket
+//
+//  Created by Mariusz Zając on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LiveMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
